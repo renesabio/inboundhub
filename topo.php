e5afdd022ad8cda2f5797441a3281629b72ed165
Renê Sábio <87675601+renesabio@users.noreply.github.com>
2026-02-01 20:41:18 -0300

    Update topo.php

--- topo.php
+++ topo.php
@@ -1,3 +1,12 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-T99B7SCDSJ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-T99B7SCDSJ');
+</script>
+
 <nav class="bg-white border-b border-slate-200 sticky top-0 z-50">
     <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
